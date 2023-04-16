4ddf095ed4453c6e6bdfefb54f70f5c2437257e4
rohithaj <130878145+rohithaj@users.noreply.github.com>
2023-04-15 18:43:44 -0700

    Update Organization.ts
    Implemented Hire function to hire an employee for  specified title

--- typescript/Organization.ts
+++ typescript/Organization.ts
@@ -1,9 +1,10 @@
 import Position from './Position';
 import Name from './Name';
+import Employee from './Employee';
 
 abstract class Organization {
   private root: Position;
-
+  private identifier:number=1;
   constructor() {
     this.root = this.createOrganization();
   }
@@ -22,7 +23,22 @@ abstract class Organization {
   // Return the newly filled position or undefined if no position has that title
   hire(person: Name, title: string): Position | undefined {
     // your code here
-    return undefined
+   return this.hireRecursion(person,title,this.root);
+  }
+
+  private hireRecursion(person: Name, title: string,position:Position):Position | undefined 
+  {
+    if(position.getTitle()===title)
+    {
+      position.setEmployee(new Employee(this.identifier++,person));
+      return position;
+    }
+    position.getDirectReports().forEach(element => {
+      var result= this.hireRecursion(person,title,element);
+      if(result!=undefined)
+       return result;
+    });
+    return undefined;
   }
 
   toString = () => this.printOrganization(this.root, '');
